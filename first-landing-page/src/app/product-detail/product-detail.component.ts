a3a04c8e45f37f6233aed33c122065f1e6868028
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2024-11-19 19:44:39 -0300

    landing reactiva

--- first-landing-page/src/app/product-detail/product-detail.component.ts
+++ first-landing-page/src/app/product-detail/product-detail.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import { Product, productList } from '../products/product.mock';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
 
+export class ProductDetailComponent implements OnInit{
+
+  producto?: Product;
+  productList: Product[] = productList;
+  loading: boolean= true;
+  
+  constructor(private _route: ActivatedRoute){}
+
+  ngOnInit(): void {
+   setTimeout(()=>{
+    this._route.params.subscribe(params=>{
+      // this.producto = params['productsId'];
+      this.producto = this.productList.find(producto=> producto.id == params['productsId']);
+      this.loading = false;
+    });
+   },1500)
+  }
 }
