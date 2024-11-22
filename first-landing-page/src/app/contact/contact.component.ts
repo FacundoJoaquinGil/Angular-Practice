49375e54f319fe2aa74aefe1477802c998343ee7
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2024-11-22 20:08:56 -0300

    formularios reactivos

--- first-landing-page/src/app/contact/contact.component.ts
+++ first-landing-page/src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+
+  myForm: FormGroup
+
+  constructor(private form: FormBuilder){
+
+    this.myForm = this.form.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+    })
+  }
+
+  hasError(controlName: string, errorType: string){
+    return this.myForm.get(controlName)?.hasError( errorType) && this.myForm.get(controlName)?.touched
+  }
+ 
+  submit(){
+    console.log(this.myForm.value)
+  }
 
 }
