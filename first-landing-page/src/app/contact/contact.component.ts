e579026398185fb2e61017787fab1e456fb8354e
Facundo Joaquín Gil <facundojoagl@gmail.com>
2024-12-06 14:10:40 -0300

    suscripciones a formularios

--- first-landing-page/src/app/contact/contact.component.ts
+++ first-landing-page/src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
@@ -7,7 +7,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+
+export class ContactComponent implements OnInit{
+
+  usuarioActivo: any = {
+    nombre: 'Pedro',
+    email: 'hola@gmail.com'
+  }
+
+  tipoDni: string = "DNI"
 
   myForm: FormGroup
 
@@ -16,7 +24,24 @@ export class ContactComponent {
     this.myForm = this.form.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
+      dni: [''],
+      tipoDni: ['']
     })
+  }
+
+  ngOnInit(): void {
+    this.myForm.patchValue({
+      name: this.usuarioActivo.nombre,
+      email: this.usuarioActivo.email,
+    })
+
+    this.myForm.get("tipoDni")?.valueChanges.subscribe(value=> {
+      this.tipoDni = value
+    }
+      
+    )
+    this.myForm.get('name')?.disable()
+    this.myForm.get('email')?.disable()
   }
 
   hasError(controlName: string, errorType: string){
