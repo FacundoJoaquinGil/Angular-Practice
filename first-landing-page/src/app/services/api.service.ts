3884ecb1eb05133dff641a4d233d91c8d7d98db4
Facundo Joaquín Gil <facundojoagl@gmail.com>
2024-12-12 19:49:33 -0300

    fake api

--- first-landing-page/src/app/services/api.service.ts
+++ first-landing-page/src/app/services/api.service.ts
@@ -1,11 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduct } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private baseUrl = "";
+  private baseUrl = "https://fakestoreapi.com/products";
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { 
+    
+   }
+
+  public getProducts(): Observable <IProduct> {
+    return this._httpClient.get<IProduct>(this.baseUrl);
+  }
+
 }
