c1e0cebfb241ddb98521c4a965f9b970bdc0bfc5
Facundo Joaquín Gil <facundojoagl@gmail.com>
2024-12-13 12:54:19 -0300

    Test Fake Api

--- first-landing-page/src/app/services/api.service.ts
+++ first-landing-page/src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IProduct } from '../models/product.model';
+import { Category, IProduct } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +14,20 @@ export class ApiService {
     
    }
 
-  public getProducts(): Observable <IProduct> {
-    return this._httpClient.get<IProduct>(this.baseUrl);
+  public getProducts(): Observable <IProduct[]> {
+    return this._httpClient.get<IProduct[]>(this.baseUrl);
+  }
+  public getCategory(): Observable <Category[]> {
+    return this._httpClient.get<Category[]>(`${this.baseUrl}/categories`);
+  }
+
+  public getProductsWithSort(sort?: string): Observable <IProduct> {
+    const params = sort ? `?sort=${sort}` : ""
+    return this._httpClient.get<IProduct>(`${this.baseUrl}?${params}`);
+  }
+
+  public getProductsById(id: number): Observable <IProduct> {
+    return this._httpClient.get<IProduct>(`${this.baseUrl}/${id}`);
   }
 
 }
