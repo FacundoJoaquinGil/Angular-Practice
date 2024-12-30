022e8da8d423a3843d5b6a353380d400ef315b98
Facundo Joaquín Gil <facundojoagl@gmail.com>
2024-12-30 12:21:12 -0300

    Product details with asynchronous parameters

--- first-landing-page/src/app/products/product.mock.ts
+++ first-landing-page/src/app/products/product.mock.ts
@@ -1,14 +1,14 @@
-export const productList: Product[] = [
-    {id: 1, name: 'Lavandina', price: 6, description: 'Desinfectante multiusos ideal para limpieza profunda y eliminación de bacterias.'},
-    {id: 2, name: 'Detergente', price: 10, description: 'Líquido concentrado para lavar platos y utensilios de cocina, eliminando grasa y residuos.'},
-    {id: 3, name: 'Limpia Vidrios', price: 12, description: 'Producto especializado para limpiar y dar brillo a vidrios y espejos sin dejar marcas.'},
-    {id: 4, name: 'Jabón', price: 8}
-];
+// export const productList: Product[] = [
+//     {id: 1, name: 'Lavandina', price: 6, description: 'Desinfectante multiusos ideal para limpieza profunda y eliminación de bacterias.'},
+//     {id: 2, name: 'Detergente', price: 10, description: 'Líquido concentrado para lavar platos y utensilios de cocina, eliminando grasa y residuos.'},
+//     {id: 3, name: 'Limpia Vidrios', price: 12, description: 'Producto especializado para limpiar y dar brillo a vidrios y espejos sin dejar marcas.'},
+//     {id: 4, name: 'Jabón', price: 8}
+// ];
 
 
-export interface Product {
-    id: number | string;
-    name: string;
-    price: number;
-    description?: string;
-}
+// export interface Product {
+//     id: number | string;
+//     name: string;
+//     price: number;
+//     description?: string;
+// }
